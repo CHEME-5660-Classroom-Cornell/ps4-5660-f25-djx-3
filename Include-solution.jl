fca6fd9c21dad2f57f07c54ba42381b9ce38b6b4
djx-3 <114043685+djx-3@users.noreply.github.com>
2025-11-09 23:53:38 -0500

    2

--- Include-solution.jl
+++ Include-solution.jl
@@ -6,8 +6,9 @@ const _PATH_TO_DATA = joinpath(_ROOT, "data");
 # make sure all is up to date -
 using Pkg
 if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
-    Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
-    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+    Pkg.activate(".")
+    Pkg.add(url="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl")
+    Pkg.instantiate()
 end
 
 # load external packages -
